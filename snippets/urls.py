20de5eb1285c5447c193bbf3b8b6a9a84ebac1e2
deepanshu-jain1999 <deepanshuj1999@gmail.com>
2018-01-27 19:33:31 +0530

    function based views working

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
+app_name = 'snippets'
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
